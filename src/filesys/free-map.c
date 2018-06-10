001016f533173dddbde5371579203ebe54c20221
doxuanviet <vietdoxuan1996@gmail.com>
2018-06-11 06:48:17 +0900

    subdirectory

--- src/filesys/free-map.c
+++ src/filesys/free-map.c
@@ -47,11 +47,12 @@ free_map_allocate (size_t cnt, block_sector_t *sectorp)
 void
 free_map_release (block_sector_t sector, size_t cnt)
 {
-  if(sector == 256) printf("Released!!!!!!!!\n");
+  printf("Released %d!\n",sector);
   ASSERT (bitmap_all (free_map, sector, cnt));
   bitmap_set_multiple (free_map, sector, cnt, false);
   bitmap_write (free_map, free_map_file);
   used_bit -= cnt;
+  printf("Done Released %d!\n",sector);
 }
 
 /* Opens the free map file and reads it from disk. */
