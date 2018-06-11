d3460acd394b3b55acf7aa4dcad6dfd82ae84a23
doxuanviet <vietdoxuan1996@gmail.com>
2018-06-11 10:11:55 +0900

    subdirectory

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -81,7 +81,6 @@ void get_args(void *esp, int *args, int cnt)
 static void
 syscall_handler (struct intr_frame *f) 
 {
-  printf("Start syscall\n");
   int args[4];
   void *esp = f->esp;
   int call_num = get_arg(esp);
@@ -92,109 +91,109 @@ syscall_handler (struct intr_frame *f)
   }
   else if(call_num == SYS_EXIT)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_EXIT\n");
     get_args(esp, args, 1);
     exit(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_EXIT\n");
   }
   else if(call_num == SYS_EXEC)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_EXEC\n");
     get_args(esp, args, 1);
     check_valid_str(args[0]);
     f->eax = exec(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_EXEC\n");
   }
   else if(call_num == SYS_WAIT)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_WAIT\n");
     get_args(esp, args, 1);
     f->eax = wait(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_WAIT\n");
   }
   else if(call_num == SYS_CREATE)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_CREATE\n");
     get_args(esp, args, 2);
     check_valid_str(args[0]);
     f->eax = create(args[0], args[1]);
-    printf("End readdir\n");
+    printf("End SYS_CREATE\n");
   }
   else if(call_num == SYS_REMOVE)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_REMOVE\n");
     get_args(esp, args, 1);
     check_valid_str(args[0]);
     f->eax = remove(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_REMOVE\n");
   }
   else if(call_num == SYS_OPEN)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_OPEN\n");
     get_args(esp, args, 1);
     check_valid_str(args[0]);
     f->eax = open(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_OPEN\n");
   }
   else if(call_num == SYS_FILESIZE)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_FILESIZE\n");
     get_args(esp, args, 1);
     f->eax = filesize(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_FILESIZE\n");
   }
   else if(call_num == SYS_READ)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_READ\n");
     get_args(esp, args, 3);
     check_valid_buffer(args[1], args[2]);
     f->eax = read(args[0], args[1], args[2]);
-    printf("End readdir\n");
+    printf("End SYS_READ\n");
   }
   else if(call_num == SYS_WRITE)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_WRITE\n");
     get_args(esp, args, 3);
     check_valid_buffer(args[1], args[2]);
     f->eax = write(args[0], args[1], args[2]);
-    printf("End readdir\n");
+    printf("End SYS_WRITE\n");
   }
   else if(call_num == SYS_SEEK)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_SEEK\n");
     get_args(esp, args, 2);
     seek(args[0], args[1]);
-    printf("End readdir\n");
+    printf("End SYS_SEEK\n");
   }
   else if(call_num == SYS_TELL)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_TELL\n");
     get_args(esp, args, 1);
     f->eax = tell(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_TELL\n");
   }
   else if(call_num == SYS_CLOSE)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_CLOSE\n");
     get_args(esp, args, 1);
     close(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_CLOSE\n");
   }
   else if(call_num == SYS_CHDIR)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_CHDIR\n");
     get_args(esp, args, 1);
     check_valid_str(args[0]);
     f->eax = chdir(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_CHDIR\n");
   }
   else if(call_num == SYS_MKDIR)
   {
-    printf("Start readdir\n");
+    printf("Start SYS_MKDIR\n");
     get_args(esp, args, 1);
     check_valid_str(args[0]);
     f->eax = mkdir(args[0]);
-    printf("End readdir\n");
+    printf("End SYS_MKDIR\n");
   }
   else if(call_num == SYS_READDIR)
   {
@@ -223,7 +222,6 @@ syscall_handler (struct intr_frame *f)
   	printf("Not known (yet) syscall.\n");
   	thread_exit ();
   }
-  printf("End syscall\n");
 }
 
 void halt (void)
